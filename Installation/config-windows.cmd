175e8fcb03c37c6f93593b92af0288b97d7ae132
rbizoi <razvan.bizoi@gmail.com>
2021-11-25 11:25:55 +0100

    Create config-windows.cmd

---
+++ Installation/config-windows.cmd
@@ -0,0 +1,4 @@
+cd c:\
+pip install sql cx_Oracle ipython-sql
+
+curl --output instantclient-basic-windows.x64-21.3.0.0.0.zip --url https://download.oracle.com/otn_software/nt/instantclient/213000/instantclient-basic-windows.x64-21.3.0.0.0.zip
