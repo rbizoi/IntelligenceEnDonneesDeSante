8f87bb9bc58003b8aa63884cabbe827e596737ca
rbizoi <razvan.bizoi@gmail.com>
2021-12-09 14:06:19 +0100

    Update config-macos.sh

--- Installation/config-macos.sh
+++ Installation/config-macos.sh
@@ -10,3 +10,10 @@ cat <<FIN_FICHIER >> ~/.profile
 LD_LIBRARY_PATH=~/instantclient_19_8:\$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH
 FIN_FICHIER
+
+
+export PATH=~/instantclient_19_8:$PATH
+
+export ARCHFLAGS="-arch x86_64"
+python -m pip install cx_Oracle --upgrade --user
+
