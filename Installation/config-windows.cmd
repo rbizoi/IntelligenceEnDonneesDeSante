e1f445009cfebd2f5ae4c6fb25596a65b75349e6
rbizoi <razvan.bizoi@gmail.com>
2023-12-05 22:07:51 +0100

    Delete config-windows.cmd

--- Installation/config-windows.cmd
+++
@@ -1,8 +0,0 @@
-cd c:\
-pip install sql cx_Oracle ipython-sql
-
-curl --output instantclient-basic-windows.x64-21.3.0.0.0.zip --url https://download.oracle.com/otn_software/nt/instantclient/213000/instantclient-basic-windows.x64-21.3.0.0.0.zip
-
-tar -xf instantclient-basic-windows.x64-21.3.0.0.0.zip
-
-dir C:\instantclient_21_3
